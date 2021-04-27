14ff719162bee6ec9e8b6befe812a3e8d139c84c
gudaofq <2873908996@qq.com>
2021-04-27 22:48:54 +0800

    黑方块移动

---
+++ src/main/java/org/gudao/Main.java
@@ -0,0 +1,17 @@
+package org.gudao;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @Auther: Gudao
+ * @Date: 2021/4/27
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        // 创建窗口对象
+        Frame f = new TankFrame();
+    }
+}
--- src/main/java/org/gudao/TankFrame.java
+++ src/main/java/org/gudao/TankFrame.java
@@ -10,6 +10,8 @@ import java.awt.event.WindowEvent;
  * @Description:
  */
 public class TankFrame extends Frame {
+    int x = 200,y = 200;
+
     public TankFrame() throws HeadlessException {
         // 设置窗口大小
         setSize(800,600);
@@ -19,6 +21,14 @@ public class TankFrame extends Frame {
         setTitle("Gudao");
         // 设置可见
         setVisible(true);
+
+        // 设置窗口可关闭：设置windows的监听类
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 
 
@@ -29,18 +39,9 @@ public class TankFrame extends Frame {
      */
     @Override
     public void paint(Graphics g) {
-        g.fillRect(200,200,50,50);
-    }
-
-    public static void main(String[] args) {
-        // 创建窗口对象
-        Frame f = new TankFrame();
-        // 设置窗口可关闭：设置windows的监听类
-        f.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        // 每次缩放窗口大小都会调用此方法，坐标也就会一直跟随改变
+        g.fillRect(x,y,50,50);
+        x += 10;
+        y += 10;
     }
 }
