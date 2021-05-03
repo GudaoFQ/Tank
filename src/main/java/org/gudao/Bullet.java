46894d6920b9313da77c7dbe4ff783f09f6f2e1e
gudaofq <2873908996@qq.com>
2021-05-03 22:15:44 +0800

    定义子弹类，设定子弹方法，在画布中将子弹画出来

--- src/main/java/org/gudao/Bullet.java
+++ src/main/java/org/gudao/Bullet.java
@@ -4,35 +4,36 @@ import java.awt.*;
 
 /**
  * @Auther: Gudao
- * @Date: 2021/4/28
+ * @Date: 2021/5/3
  * @Description:
  */
-public class Tank {
+public class Bullet {
+    private static final int SPEED = 10;
     private int x,y;
-    private Dir dir = Dir.DOWN;
-    private static final int SPEED = 5;
-    private boolean moving = false;
+    private Dir dir;
+    private static int  WIDTH = 5,HEIGHT = 5;
 
-    public Tank(int x, int y, Dir dir) {
-        super();
+    public Bullet(int x, int y, Dir dir) {
         this.x = x;
         this.y = y;
         this.dir = dir;
     }
 
     public void paint(Graphics g) {
-        g.fillRect(x,y,50,50);
+        Color color = g.getColor();
+        // 设置子弹颜色
+        g.setColor(Color.red);
+        // 设置子弹大小与位置
+        g.fillOval(x,y,WIDTH,HEIGHT);
+        g.setColor(color);
         move();
     }
 
     /**
-     * 坦克移动方法
+     * 子弹移动方法
      */
     private void move() {
-        if(!moving){
-            return;
-        }
-        switch (dir){
+        switch (dir) {
             case LEFT:
                 x -= SPEED;
                 break;
@@ -48,24 +49,5 @@ public class Tank {
             default:
                 break;
         }
-    }
-
-
-    /**
-     * 坦克方向设置
-     *
-     * @param dir dir
-     */
-    public void setDir(Dir dir) {
-        this.dir = dir;
-    }
-
-    /**
-     * 坦克是移动设置
-     *
-     * @param moving 移动
-     */
-    public void setMoving(boolean moving) {
-        this.moving = moving;
     }
 }
--- src/main/java/org/gudao/TankFrame.java
+++ src/main/java/org/gudao/TankFrame.java
@@ -13,6 +13,7 @@ import java.awt.event.WindowEvent;
  */
 public class TankFrame extends Frame {
     Tank tank = new Tank(200,200,Dir.DOWN);
+    Bullet bullet = new Bullet(300,300,Dir.DOWN);
 
     public TankFrame() throws HeadlessException {
         // 设置窗口大小
@@ -47,7 +48,7 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g) {
         tank.paint(g);
-
+        bullet.paint(g);
     }
 
 
